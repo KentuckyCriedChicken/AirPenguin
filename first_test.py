b48302e83e573e9ce911c01eeb5fa0637c352fd5
KentuckyCriedChicken <142221105+KentuckyCriedChicken@users.noreply.github.com>
2023-08-16 14:28:19 +0300

    Add files via upload

---
+++ first_test.py
@@ -0,0 +1,9 @@
+import customtkinter
+
+app = customtkinter.CTk()
+app.title("Air Penguin Reservation")
+app.geometry("900x700")
+app.iconbitmap("C:/Users/Mohammed Kaif/Downloads/Screenshot 2023-08-16 132550.ico")
+
+
+app.mainloop()
