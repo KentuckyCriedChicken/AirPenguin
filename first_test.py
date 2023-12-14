e8e3daf5da0f4a68e7abf206664392d388e958e0
KentuckyCriedChicken <142221105+KentuckyCriedChicken@users.noreply.github.com>
2023-12-14 18:39:02 +0300

    Update first_test.py

--- first_test.py
+++ first_test.py
@@ -1,9 +1,12 @@
 import customtkinter
 
 app = customtkinter.CTk()
-app.title("Air Penguin Reservation")
+app.title("Fly Penguin")
 app.geometry("900x700")
 app.iconbitmap("C:/Users/Mohammed Kaif/Downloads/Screenshot 2023-08-16 132550.ico")
+app.grid_columnconfigure(0, weight=1)
 
+label = customtkinter.CTkLabel(app, text="Welcome To Fly Penguin", fg_color="transparent", text_color="white")
+label.grid(row=0, column=0, padx=20, pady=20)
 
 app.mainloop()
