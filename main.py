3a1af89f24d6c66d030ea22977fb902519f784b5
KentuckyCriedChicken <142221105+KentuckyCriedChicken@users.noreply.github.com>
2023-12-15 10:07:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,33 @@
 import customtkinter
 
+app = customtkinter.CTk()
+app.title("Fly Penguin")
+app.geometry("900x700")
+
+#4 by 4 grid 
+for i in range(4):
+     app.columnconfigure(i, weight=1)
+
+#tabs
+tabview = customtkinter.CTkTabview(app)
+tabview.grid(row=0, column=0)
+
+tabview.add("tab 1")  # add tab at the end
+tabview.add("tab 2")  # add tab at the end
+tabview.set("tab 2")  # set currently visible tab
 
 
-def Search_Flights():
-    app.destroy()
+main_title = customtkinter.CTkLabel(tabview.tab("tab 2"), text="Welcome To Fly Penguin",text_color="white", font=("Bahnschrift",60) )
+main_title.grid(row=1, column=1, padx=20, columnspan=2, pady=20)
 
-    #initialising
-    apps = customtkinter.CTk()
-    apps.title("Fly Penguin")
-    apps.geometry("900x700")
+#buttons
+main_button_searchflights = customtkinter.CTkButton(tabview.tab("tab 2"), text="Search Flights", font=("Bahnschrift", 30), height=60, width=250)
+main_button_searchflights.grid(row=2, column=1, columnspan=2)
 
-    #4 by 4 grid
-    for i in range(4):
-        apps.columnconfigure(i, weight=1)
+main_button_bookflight = customtkinter.CTkButton(tabview.tab("tab 2"), text="Book Flight", font=("Bahnschrift", 30), height=60, width=250)
+main_button_bookflight.grid(row=3, column=1, columnspan=2)
 
-    search_title = customtkinter.CTkLabel(apps, text="Search Flights",text_color="white", font=("Bahnschrift",60) )
-    search_title.grid(row=1, column=1, padx=20, columnspan=2, pady=20)
+main_button_cancel = customtkinter.CTkButton(tabview.tab("tab 2"), text="Cancel Booking", font=("Bahnschrift", 30), height=60, width=250)
+main_button_cancel.grid(row=4, column=1, columnspan=2)
 
-    search_frame = customtkinter.CTkFrame(apps, width=600, height=500)
-    search_frame.grid(row=2, column=0, rowspan=3, columnspan=4, pady=20)
-    
-
-    apps.mainloop()
-
-def Main_Menu():
-    global app
-    #initialising
-    app = customtkinter.CTk()
-    app.title("Fly Penguin")
-    app.geometry("900x700")
-    #4 by 4 grid 
-    for i in range(4):
-        app.columnconfigure(i, weight=1)
-
-
-    main_title = customtkinter.CTkLabel(app, text="Welcome To Fly Penguin",text_color="white", font=("Bahnschrift",60) )
-    main_title.grid(row=1, column=1, padx=20, columnspan=2, pady=20)
-
-    main_frame = customtkinter.CTkFrame(app, width=600, height=500)
-    main_frame.grid(row=2, column=0, rowspan=3, columnspan=4, pady=20)
-
-    #buttons
-    main_button_searchflights = customtkinter.CTkButton(app, text="Search Flights", font=("Bahnschrift", 30), height=60, width=250, command=Search_Flights)
-    main_button_searchflights.grid(row=2, column=1, columnspan=2)
-
-    main_button_bookflight = customtkinter.CTkButton(app, text="Book Flight", font=("Bahnschrift", 30), height=60, width=250)
-    main_button_bookflight.grid(row=3, column=1, columnspan=2)
-
-    main_button_cancel = customtkinter.CTkButton(app, text="Cancel Booking", font=("Bahnschrift", 30), height=60, width=250)
-    main_button_cancel.grid(row=4, column=1, columnspan=2)
-
-    app.mainloop()
-
-Main_Menu()
-
-  
+app.mainloop()
